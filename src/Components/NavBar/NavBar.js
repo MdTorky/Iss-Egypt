f99002dce86daefaa2e8ba33c8cef2f423ff7598
cargo <mohamed2003torky@gmail.com>
2023-09-01 19:12:26 +0300

    First Commit

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">Home</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
